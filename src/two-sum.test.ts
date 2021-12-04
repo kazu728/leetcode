f304b574189c090dd97f613e29f1cbe1092eadbc
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2021-12-04 00:23:33 -0800

    feat: two-sum

---
+++ src/two-sum.test.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from "./deps.ts";
+import { twoSum } from "./two-sum.ts";
+
+Deno.test("Should return right index in case of target is 9", () => {
+  const nums = [2, 7, 11, 15];
+  const target = 9;
+
+  assertEquals(twoSum(nums, target), [0, 1]);
+});
+
+Deno.test("Should return right index in case of target is 6", () => {
+  const nums = [3, 2, 4];
+  const target = 6;
+
+  assertEquals(twoSum(nums, target), [1, 2]);
+});
+
+Deno.test("Should return right index in case of target is 6", () => {
+  const nums = [3, 3];
+  const target = 6;
+
+  assertEquals(twoSum(nums, target), [0, 1]);
+});
