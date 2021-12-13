168edacd20ea2679bdb98d0cec0685aa0b71f117
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2021-12-13 15:18:49 -0800

    refactor: maximum-depth-of-binary-tree

--- src/tree/maximum-depth-of-binary-tree.ts
+++ src/tree/maximum-depth-of-binary-tree.ts
@@ -1,10 +1,7 @@
 import { MaybeNode } from "./treenode.ts";
 
 export function maxDepth<T>(root: MaybeNode<T>): number {
-  if (root === null) return 0;
-
-  const leftDepth = 1 + maxDepth(root.left);
-  const rightDepth = 1 + maxDepth(root.right);
-
-  return Math.max(leftDepth, rightDepth);
+  return root === null
+    ? 0
+    : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
